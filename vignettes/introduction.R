9806428155066a8a4abf932cc833141f77badb31
Andy Madrid <76069657+andymadrid@users.noreply.github.com>
2024-05-10 15:24:01 -0500

    Add files via upload

---
+++ vignettes/introduction.R
@@ -0,0 +1,76 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## -----------------------------------------------------------------------------
+### Install required packages
+# install.packages(c("devtools","bsseq","glmnet", "mice"))
+
+
+### install the madrid package
+# install_github("andymadrid/madrid")
+
+
+
+## ----setup--------------------------------------------------------------------
+# load in the madrid pacakge
+suppressPackageStartupMessages(library(madrid))
+
+
+
+## -----------------------------------------------------------------------------
+# get toy example
+data(bs)
+
+
+
+## -----------------------------------------------------------------------------
+# summary of bsseq object
+bs
+
+# summary of phenotypic data associated with bsseq object
+pData(bs)
+
+
+
+## -----------------------------------------------------------------------------
+# guess sample sex from bsseq object
+guessed_sex <- guessSex(bs)
+
+# look at the output
+guessed_sex
+
+
+
+## -----------------------------------------------------------------------------
+# predict age from bsseq object
+predicted_age <- predictAge(bs)
+
+# look at the output
+predicted_age
+
+
+
+## -----------------------------------------------------------------------------
+# assess the correlation between actual and estimated age
+cor(predicted_age$predictedAge, pData(bs)$Age)
+
+
+
+## -----------------------------------------------------------------------------
+# test for differences (accelerated/decelerated) in actual and estimated age
+predicted_age$difference <- predicted_age$predictedAge - pData(bs)$Age
+
+t.test(predicted_age$difference ~ factor(pData(bs)$Diagnosis))
+
+
+
+## -----------------------------------------------------------------------------
+# test for differences in the resduals between actual and estimated age
+predicted_age$residuals <- resid(lm(predicted_age$predictedAge ~ pData(bs)$Age))
+t.test(predicted_age$residuals ~ factor(pData(bs)$Diagnosis))
+
+
+
